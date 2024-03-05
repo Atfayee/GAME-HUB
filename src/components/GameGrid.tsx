1e8b40c1310b7c6b4961976b7d8785d8b87eb3c5
Atfayee <jingchi.zhang@outlook.com>
2024-03-04 19:56:58 -0500

    Create a generic data fetching hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,12 +3,12 @@ import useGames from "./hooks/useGames";
 import { SimpleGrid, Text } from "@chakra-ui/react";
 
 function GameGrid() {
-  const { games, error } = useGames();
+  const { data, error, isLoading } = useGames();
   return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing="10">
-        {games.map((game) => (
+        {data.map((game) => (
           <GameCard key={game.id} game={game}></GameCard>
         ))}
       </SimpleGrid>
